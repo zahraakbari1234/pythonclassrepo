93f2cca0859aa5e02fb3c5981d4cfa30ddfa5c09
zahraakbari1234 <zhra.akbari@gmail.com>
2023-06-08 19:10:22 +0330

    3:30

--- neon_exp/neon_1/first_exp.py
+++ neon_exp/neon_1/first_exp.py
@@ -1,1 +1,10 @@
 print("test")
+x=1+1
+a=979+5
+temp = 35
+if temp > 30 :
+    a=10
+    
+a = "10" if temp > 30 else "0" # a = temp > 30 ? 10 : 0    not correct
+if 20 < temp < 30 : # if temp > 20 and temp < 30 also correct
+    print("normal")
