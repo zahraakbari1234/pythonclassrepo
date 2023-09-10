d6121879a186fae8475df8576e36bc458fa69020
zahraakbari1234 <zhra.akbari@gmail.com>
2023-09-10 20:04:07 +0330

    matrix exa

--- matrix.py
+++ matrix.py
@@ -1,29 +1,37 @@
 
 def main():
     data = [ [ 0 for i in range(3) ] for j in range(3) ]
+
+    i = 0
+    j = 0
     
-    for i in range(3):
-        for j in range(3):
-            try:
+    while( i < 3 ):
+        while( j < 3 ):
+             try:
                 data[i][j] = int ( input(('Enter data[%d][%d]:' %(i,j) )) )
-            except:
+                j += 1
+             except:
                 print('Error!Enter Integer Value!')
-                data[i][j] = int ( input(('Enter data[%d][%d]:' %(i,j) )) )
+        i += 1
+        j = 0
+       
 
 
-    
-    
-    result = ( '''result: |  %d %d %d  |
+    result = ( '''result:
         |  %d %d %d  |
-        |  %d %d %d  |'''           %(data[0][0],data[0][1],data[0][2],
-                                      data[1][0],data[1][1],data[1][2],
-                                      data[2][0],data[2][1],data[2][2]) )
-    print(20*'-')
+        |  %d %d %d  |
+        |  %d %d %d  |'''  %(data[0][0],data[0][1],data[0][2],
+                           data[1][0],data[1][1],data[1][2],
+                           data[2][0],data[2][1],data[2][2]) )
+    print( )
+    print( )
+    print( )
+    print( 20 * '-' )
+
     print( result )
 
-    
-    input()
 
+    input('Press Enter To Exit')
 
-                                
+                              
 if __name__ == "__main__":main()
